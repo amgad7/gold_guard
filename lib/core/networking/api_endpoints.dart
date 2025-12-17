b032449b76d5c4ac4ee8205dd4be2d84d7d4bd46
amgad ahmed <13239659amgad@gmail.com>
2025-12-17 18:21:46 +0200

    edit apiKey

--- lib/core/networking/api_endpoints.dart
+++ lib/core/networking/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String apiKey = 'SONS9Y1NQ0LMBCC6IKJA287C6IKJA';
+  static const String apiKey = 'CURZLAGEGEV1PTSGN3OS929SGN3OS';
   static const String exchangeRateUrl =
       'https://api.exchangerate-api.com/v4/latest/USD';
 }
